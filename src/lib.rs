7c9a03d7d396eebf215beff5fea934ba593f3914
Vinod Akshat <pumkininriver@gmail.com>
2025-05-17 00:11:37 +0530

    Message to bytes + Resorce Record struct for better parsing and understanding

--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,21 @@ pub struct DnsQuestion {
     pub qclass: u16,   // IN = 1
 }
 #[derive(Debug)]
+pub struct ResourceRecord {
+    pub name: String,
+    pub rr_type: u16, // A = 1, NS = 2, etc.
+    pub class: u16,   // Usually IN (1)
+    pub ttl: u32,
+    pub rdlength: u16,
+    pub rdata: Vec<u8>, // Parsed separately depending on type
+}
+#[derive(Debug)]
 pub struct DnsMessage {
     pub header: DnsHeader,
     pub question: DnsQuestion,
-    pub answers: Vec<u8>,    // RRs in response to query
-    pub authority: Vec<u8>,  //Records for authoritative servers
-    pub additional: Vec<u8>, //Additional helpful info
+    pub answers: Vec<ResourceRecord>,    // RRs in response to query
+    pub authority: Vec<ResourceRecord>,  //Records for authoritative servers
+    pub additional: Vec<ResourceRecord>, //Additional helpful info
 }
 
 impl DnsMessage {
@@ -50,6 +59,34 @@ impl DnsMessage {
             authority: Vec::new(),
             additional: Vec::new(),
         }
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+
+        // HEADER SECTION
+        bytes.extend(&self.header.identification.to_be_bytes()); // 2 bytes
+        bytes.extend(&self.header.flags.to_be_bytes()); // 2 bytes
+        bytes.extend(&self.header.no_of_questions.to_be_bytes()); // 2 bytes
+        bytes.extend(&self.header.no_of_answers_rr.to_be_bytes()); // 2 bytes
+        bytes.extend(&self.header.no_of_authority_rr.to_be_bytes()); // 2 bytes
+        bytes.extend(&self.header.no_of_additional_rr.to_be_bytes()); // 2 bytes
+
+        // QUESTION SECTION
+        // QNAME — example.com becomes [7]example[3]com[0]
+        for label in self.question.qname.split('.') {
+            bytes.push(label.len() as u8); // length byte
+            bytes.extend(label.as_bytes()); // label bytes
+        }
+        bytes.push(0); // end of QNAME
+
+        // QTYPE (2 bytes)
+        bytes.extend(&self.question.qtype.to_be_bytes());
+
+        // QCLASS (2 bytes)
+        bytes.extend(&self.question.qclass.to_be_bytes());
+
+        bytes
     }
 }
 
